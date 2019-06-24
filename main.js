39061b33a15a5839ba2194101853cc2ec218ed09
Vadyanga <mr.b5414@yandex.ru>
2019-06-25 00:46:15 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 $.fn.extend({KeyboardFlex:function(){
-		let ob = this[0];
-		let i = {target:ob,bubbles:!0,popGenerated:!0,isFillrWidgetEvent:!0,keyCode:19,key:"a"};
-		let E1 = new KeyboardEvent("keyup", i),E2 = new KeyboardEvent("input", i),E3 = new KeyboardEvent("blur", i);
-		if(ob){ob.dispatchEvent(E1);ob.dispatchEvent(E2);ob.dispatchEvent(E3);}
+	let ob = this[0];
+	let i = {target:ob,bubbles:!0,popGenerated:!0,isFillrWidgetEvent:!0,keyCode:19,key:"a"};
+	let E1 = new KeyboardEvent("keyup", i),E2 = new KeyboardEvent("input", i),E3 = new KeyboardEvent("blur", i);
+	if(ob){ob.dispatchEvent(E1);ob.dispatchEvent(E2);ob.dispatchEvent(E3);}
 }});
 
 
