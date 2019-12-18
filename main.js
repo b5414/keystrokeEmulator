6f691bcd08d4adcb91ab883ff5a8df4ae3c37643
Vadyanga <mr.b5414@yandex.ru>
2019-12-18 22:22:44 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 
-// Requires: Jquery
-$.fn.extend({keyboardFlex:function(){
-		let ob = this[0];
-		let i = {target:ob,bubbles:!0,popGenerated:!0,isFillrWidgetEvent:!0,keyCode:19,key:"a"};
-		let E1 = new KeyboardEvent("keyup", i),E2 = new KeyboardEvent("input", i),E3 = new KeyboardEvent("blur", i);
-		if(ob){ob.dispatchEvent(E1);ob.dispatchEvent(E2);ob.dispatchEvent(E3);}
+// requires: JQuery
+$.fn.extend({ks:function(){
+	let ob = this[0];
+	let i = {target:ob,bubbles:!0,popGenerated:!0,isFillrWidgetEvent:!0,keyCode:19,key:"a"};
+	let E1 = new KeyboardEvent("keyup", i),E2 = new KeyboardEvent("input", i),E3 = new KeyboardEvent("blur", i);
+	if(ob){ob.dispatchEvent(E1);ob.dispatchEvent(E2);ob.dispatchEvent(E3);}
 }});
 
-// Usage example:
-var value = 'Ivan';
-$('#name').val(value).keyboardFlex();
-
+// example:
+const someText = 'Sten';
+$('#namefield').val(someText).ks();
