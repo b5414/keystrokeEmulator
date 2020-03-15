91d73ac120abc3889844811dbfb8d3f61cf024a4
Vadim <mr.b5414@yandex.ru>
2020-03-15 20:08:42 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 
-// requires: JQuery
 $.fn.extend({ks:function(){
 	let ob = this[0];
 	let i = {target:ob,bubbles:!0,popGenerated:!0,isFillrWidgetEvent:!0,keyCode:19,key:"a"};
 	let E1 = new KeyboardEvent("keyup", i),E2 = new KeyboardEvent("input", i),E3 = new KeyboardEvent("blur", i);
 	if(ob){ob.dispatchEvent(E1);ob.dispatchEvent(E2);ob.dispatchEvent(E3);}
 }});
-
-// example:
-const someText = 'Sten';
-$('#namefield').val(someText).ks();
